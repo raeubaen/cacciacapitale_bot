7f3c9055cb1f6235d3f9c289d21d11861b05525d
ruben <rubengargiulo@yahoo.it>
2020-02-17 15:07:25 +0100

    17/02/2020_15:07:25

--- bot_site/settings.py
+++ bot_site/settings.py
@@ -140,11 +140,6 @@ LOGOUT_REDIRECT_URL = 'home'
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
