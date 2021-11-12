15bf17d772647bb98cb6689d8a500d45f700fa84
raeubaen <gargiulo.1824800@studenti.uniroma1.it>
2021-11-12 19:40:55 +0100

    12/11/2021_19:40:55

--- Bot/admin.py
+++ Bot/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Captain, Queue, Hunter, Key, Bot_Table
+from .models import Captain, Queue, Hunter, Key, Bot_Table, AdminId
 
 class Hunter_Inline(admin.StackedInline):
     model = Hunter
 
 class Queue_Inline(admin.StackedInline):
     model = Queue
+
+class AdminId_Inline(admin.StackedInline):
+    model = AdminId
 
 class Captain_Admin(admin.ModelAdmin):
     inlines = [
@@ -19,8 +22,13 @@ class Hunter_Admin(admin.ModelAdmin):
         Queue_Inline,
     ]
 
+class Bot_Table_Admin(admin.ModelAdmin):
+    inlines = [
+        AdminId_Inline
+    ]
+
 admin.site.register(Captain, Captain_Admin)
 admin.site.register(Hunter, Hunter_Admin)
 admin.site.register(Key)
-admin.site.register(Bot_Table)
-
+admin.site.register(Bot_Table, Bot_Table_Admin)
+admin.site.register(AdminId)
