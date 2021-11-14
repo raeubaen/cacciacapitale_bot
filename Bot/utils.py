5d64f5677c6e0dbf2258946a97db93fdca41090c
raeubaen <gargiulo.1824800@studenti.uniroma1.it>
2021-11-14 14:05:17 +0100

    14/11/2021_14:05:17

--- Bot/utils.py
+++ Bot/utils.py
@@ -11,7 +11,7 @@ def already_in(chat_id):
     except Hunter.DoesNotExist:
         return False
     else:
-        is_cap = (if chat_id in Captain.objects.all().values_list("id", flat=True))
+        is_cap = (chat_id in Captain.objects.all().values_list("id", flat=True))
         is_in = hasattr(hunter, "queue") or is_cap
         if not is_in:
             hunter.delete()
